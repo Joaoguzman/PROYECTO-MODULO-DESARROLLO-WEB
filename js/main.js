c009e501a13ad088a9990c5121995be70a9ac1f4
joaoguzman <joaogg@ug.uchile.cl>
2020-10-29 20:42:16 -0300

    correccion graficos y agregar tablas

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ Chart.defaults.global.defaultFontSize = 18;
 var speedData = {
     labels: ["0s", "10s", "20s", "30s", "40s", "50s", "60s"],
     datasets: [{
-        label: "Car Speed (mph)",
+        label: "Hemograma",
         data: [0, 59, 75, 20, 20, 55, 40],
     }]
 };
@@ -15,7 +15,7 @@ var speedData = {
 var chartOptions = {
     legend: {
         display: true,
-        position: 'top',
+        position: 'bottom',
         labels: {
             boxWidth: 80,
             fontColor: 'black'
@@ -32,13 +32,13 @@ var lineChart = new Chart(speedCanvas, {
 //Perfil lipídico
 
 
-var densityCanvas = document.getElementById("densityChart");
+var densityCanvas = document.getElementById("graph_lipidico");
 
 Chart.defaults.global.defaultFontFamily = "Lato";
 Chart.defaults.global.defaultFontSize = 18;
 
 var densityData = {
-    label: 'Density of Planet (kg/m3)',
+    label: 'Valores lipídicos1',
     data: [5427, 5243, 5514, 3933, 1326, 687, 1271, 1638],
     backgroundColor: 'rgba(0, 99, 132, 0.6)',
     borderWidth: 0,
@@ -46,7 +46,7 @@ var densityData = {
 };
 
 var gravityData = {
-    label: 'Gravity of Planet (m/s2)',
+    label: 'Valores lipídicos2',
     data: [3.7, 8.9, 9.8, 3.7, 23.1, 9.0, 8.7, 11.0],
     backgroundColor: 'rgba(99, 132, 0, 0.6)',
     borderWidth: 0,
