27e2b8592a23d5d5152a13906d9426109139ed29
joaoguzman <joaogg@ug.uchile.cl>
2020-10-29 23:27:31 -0300

    correccion graficas 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //Hemograma
-var speedCanvas = document.getElementById("speedChart");
+var speedCanvas = document.getElementById("graph_hemograma");
 
 Chart.defaults.global.defaultFontFamily = "Lato";
 Chart.defaults.global.defaultFontSize = 18;
@@ -15,7 +15,7 @@ var speedData = {
 var chartOptions = {
     legend: {
         display: true,
-        position: 'bottom',
+        position: 'top',
         labels: {
             boxWidth: 80,
             fontColor: 'black'
@@ -35,11 +35,11 @@ var lineChart = new Chart(speedCanvas, {
 var densityCanvas = document.getElementById("graph_lipidico");
 
 Chart.defaults.global.defaultFontFamily = "Lato";
-Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontSize = 13;
 
 var densityData = {
     label: 'Valores lipídicos1',
-    data: [5427, 5243, 5514, 3933, 1326, 687, 1271, 1638],
+    data: [1000, 1326, 687, 1271, 1638],
     backgroundColor: 'rgba(0, 99, 132, 0.6)',
     borderWidth: 0,
     yAxisID: "y-axis-density"
@@ -47,14 +47,14 @@ var densityData = {
 
 var gravityData = {
     label: 'Valores lipídicos2',
-    data: [3.7, 8.9, 9.8, 3.7, 23.1, 9.0, 8.7, 11.0],
+    data: [3.7, 4.1, 9.0, 8.7, 11.0],
     backgroundColor: 'rgba(99, 132, 0, 0.6)',
     borderWidth: 0,
     yAxisID: "y-axis-gravity"
 };
 
 var planetData = {
-    labels: ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
+    labels: ["Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
     datasets: [densityData, gravityData]
 };
 
